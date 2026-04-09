9db1f6a59a1dcde31a0a3eb2909207c0025aae62
roby-umbra <robertcastillo@umbradigitalcompany.com>
2026-04-09 22:03:42 +0800

    initial commit

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",       // default in WAMP
-  password: "apr26optical",       // usually empty in WAMP
+  password: "",       // usually empty in WAMP
   database: "nextjs_demo",
 });
 
